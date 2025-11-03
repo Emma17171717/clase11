0ca0008a1b62ff4b45eae3a74173574a61e18c06
Emma17171717 <126958679+Emma17171717@users.noreply.github.com>
2025-11-03 17:08:48 -0500

    Update Pollos.py

--- Pollos.py
+++ Pollos.py
@@ -1,1 +1,19 @@
-#Eliana Hernandez - 02240132001
+class Animal:
+    def __init__(self, codigo, raza, edad):
+        self.codigo = codigo
+        self.raza = raza
+        self.edad = edad
+        self.huevos_semana = []
+
+    def registrar_produccion(self, cantidad):
+        self.huevos_semana.append(cantidad)
+
+    def produccion_total(self):
+        return sum(self.huevos_semana)
+
+    def mostrar_info(self):
+        print(f"Código: {self.codigo}")
+        print(f"Raza: {self.raza}")
+        print(f"Edad: {self.edad} semanas")
+        print(f"Huevos por semana: {self.huevos_semana}")
+        print(f"Producción total: {self.produccion_total()} huevos\n")
